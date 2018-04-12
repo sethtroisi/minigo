cfc2f072463ce6f95f625bee4c3a7f7127ac9ff7
Seth Troisi <braintwo@gmail.com>
2018-04-11 20:58:33 -0700

    position_pv

---
+++ oneoffs/position_pv.py
@@ -0,0 +1,99 @@
+"""
+Used to find PV for position.
+Check FLAGS for default values.
+
+Usage:
+python position_pv.py
+
+"""
+import sys; sys.path.insert(0, '.')
+
+import itertools
+import os
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+import go
+import strategies
+import oneoff_utils
+
+
+tf.app.flags.DEFINE_string(
+    "sgf_dir", "sgf/baduk_db/", "sgf database")
+
+tf.app.flags.DEFINE_string("model_dir", "saved_models",
+                           "Where the model files are saved")
+tf.app.flags.DEFINE_string("data_dir", "data/eval", "Where to save data")
+tf.app.flags.DEFINE_integer("idx_start", 150,
+                            "Only take models after given idx")
+tf.app.flags.DEFINE_integer("eval_every", 5,
+                            "Eval every k models to generate the curve")
+tf.app.flags.DEFINE_integer("readouts", 1000,
+                            "Eval every k models to generate the curve")
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def eval_pv(eval_positions, model_dir, data_dir, idx_start, eval_every, readouts):
+  model_paths = oneoff_utils.get_model_paths(model_dir)
+
+  print("Evaluating models {}-{}, eval_every={}".format(
+        idx_start, len(model_paths), eval_every))
+  for idx in tqdm(range(idx_start, len(model_paths), eval_every)):
+    if idx == idx_start:
+      player = oneoff_utils.load_player(model_paths[idx])
+    else:
+      oneoff_utils.restore_params(model_paths[idx], player)
+
+    mcts = strategies.MCTSPlayerMixin(
+        player.network,
+        simulations_per_move=readouts,
+        resign_threshold=-1)
+
+    eval_trees = []
+    for pos_name, position in eval_positions:
+        mcts.initialize_game(position)
+        mcts.suggest_move(position)
+
+        path = []
+        node = mcts.root
+        while node.children:
+            next_kid = np.argmax(node.child_N)
+            node = node.children.get(next_kid)
+            path.append("{},{}".format(node.fmove, int(node.N)))
+
+        save_file = os.path.join(data_dir, "pv-{}-{}".format(pos_name, idx))
+        with open(save_file, "w") as data:
+            data.write("{},  {}\n".format(idx, ",".join(path)))
+
+
+def positions_from_sgfs(sgf_files):
+  # sgf_replay doesn't like empty sgf file
+  data = [("empty", go.Position(komi=7.5))]
+  for sgf in sgf_files:
+    sgf_name = os.path.basename(sgf).replace(".sgf", "")
+    positions, moves, _ = oneoff_utils.parse_sgf(sgf)
+    final = positions[-1].play_move(moves[-1])
+    data.append((sgf_name, final))
+  return data
+
+
+def main(unusedargv):
+  sgf_files = oneoff_utils.find_and_filter_sgf_files(FLAGS.sgf_dir)
+  eval_positions = positions_from_sgfs(sgf_files)
+
+  eval_pv(
+      eval_positions,
+      FLAGS.model_dir,
+      FLAGS.data_dir,
+      FLAGS.idx_start,
+      FLAGS.eval_every,
+      FLAGS.readouts)
+
+
+FLAGS = tf.app.flags.FLAGS
+
+if __name__ == "__main__":
+  tf.app.run(main)
